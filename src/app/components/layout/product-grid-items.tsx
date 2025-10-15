ad013c58ebf8c4126c94d18628ce6288d31ac44f
Mingsheng Tang <nathantang@Mac.lan>
2025-10-15 21:36:54 +1100

    update product grid items

--- src/app/components/layout/product-grid-items.tsx
+++ src/app/components/layout/product-grid-items.tsx
@@ -1,1 +1,14 @@
-export default function ProductGridItems()
+import { Product } from "@/app/lib/shopify/types";
+import { Grid } from "lucide-react";
+
+export default function ProductGridItems(
+    {products}: {products: Product[]}
+){
+    return(
+        <>
+        {products.map(product) =>(
+            <Grid.Item key={product.handle}></Grid.Item>
+        )}
+        </>
+    )
+}
